2704edbca43fcc1220eeb6dc4360258a39b3475d
xJaguar25x <22175098+xJaguar25x@users.noreply.github.com>
2020-12-20 19:41:18 +0300

    clean app

---
+++ src/containers/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
